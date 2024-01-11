ebbedd72ddeb437ba7759d37e939a8ec7c74c387
yelo-o <flyordig@gmail.com>
2024-01-12 00:58:57 +0900

    c++ 기본 구조

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+#include <iostream> // iostream이라는 헤더를 포함
 
-using namespace std;
+using namespace std; // 네임스페이스 설명 std::cout
 
-int main()
+int main() // entry point
 {
   cout << "Hello, World" << endl;
+  // printf("Hello World!!! by printf");
 
   return 0;
 }
